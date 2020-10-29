0dbd02c669f162a6a945ef1511a2b8d4c6b3c322
ZeusAbhijeet <62468954+ZeusAbhijeet@users.noreply.github.com>
2020-10-29 17:07:35 +0530

    Update Util.py

--- Util.py
+++ Util.py
@@ -3,14 +3,54 @@ from discord.ext import commands
 import asyncio
 import os
 import sqlite3
+from dotenv import load_dotenv
+from datetime import datetime
+from datetime import date
+
+load_dotenv()
+LOG = os.getenv('LOG_ID')
 
 POINT = {}
 
 conn = sqlite3.connect('Database.db')
 c = conn.cursor()
 
-c.execute("SELECT user_id, points FROM point_table")
+c.execute("SELECT user_id, points FROM point_table;")
 DB_POINT = c.fetchall()
 
 conn.close()
+
+async def Backup(client):
+	await client.wait_until_ready()
+	global POINT
+	global DB_POINT
+	while not client.is_closed():
+		await client.get_channel(LOG).send(f"Backup OK: '''{datetime.now}'''")
+		# Repeat every 1 hour
+		await asyncio.sleep(3600)
+		conn = sqlite3.connect('Database.db')
+		c = conn.cursor()
+
+		c.execute("SELECT user_id, points FROM point_table;")
+		DB_POINT = c.fetchall()
+
+		await client.get_channel(LOG).send(f"Performing Backup: ```{datetime.now()}```")
+		# POINT BACKUP
+		for user in DB_POINT:
+			if user[0] in POINT:
+				c.execute("UPDATE point_table SET points = {} WHERE user_id = {};".format(POINT[user[0]]+user[1], user[0]))
+		is_instance = False
+		for user in POINT:
+			for elm in DB_POINT:
+				if user in elm:
+					is_instance = True
+					break
+			if not is_instance:
+				c.execute("INSERT INTO point_table VALUES (NULL, {}, {};".format(user, POINT[user]))
+			is_instance = False
+		
+		conn.commit()
+		conn.close()
+
+		POINT = {}
 
