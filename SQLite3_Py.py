47416ee3e7b7eab90480be66bc8e8ba87f27e7f2
ZeusAbhijeet <62468954+ZeusAbhijeet@users.noreply.github.com>
2020-10-31 21:01:46 +0530

    Update SQLite3_Py.py

--- SQLite3_Py.py
+++ SQLite3_Py.py
@@ -3,15 +3,16 @@ import sqlite3
 if __name__ == '__main__':
 	conn = sqlite3.connect('Database.db')
 	c = conn.cursor()
-	#sql_cmd = open("./SQL_CMDS/c-point_table.txt", 'r').read()
-	#c.execute(sql_cmd)
+	sql_cmd = open("./SQL_CMDS/c-point_table.txt", 'r').read()
+	c.execute(sql_cmd)
 	print("Created Point Table")
-	#sql_cmd = open("./SQL_CMDS/c-channel_table.txt", 'r').read()
-	#c.execute(sql_cmd)
+	sql_cmd = open("./SQL_CMDS/c-channel_table.txt", 'r').read()
+	c.execute(sql_cmd)
 	print("Created Channel Table")
 
 	sql_cmd = open("./SQL_CMDS/i-channel_table.txt", 'r').read()
 	c.execute(sql_cmd)
+	print("Init into channel_table")
 
 	conn.commit()
 	conn.close()
