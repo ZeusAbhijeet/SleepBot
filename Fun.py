a20d5c4c81e2e8830e3080289826f895c04c4c2f
1,094,795,585 <33022492+9192939495969798@users.noreply.github.com>
2020-10-31 13:40:16 +0530

    Update Fun.py

--- Fun.py
+++ Fun.py
@@ -18,9 +18,9 @@ class Fun(commands.Cog):
 	@commands.command(name='ban')
 	@commands.cooldown(1, 300, commands.BucketType.user)
 	async def ban(self, ctx, target : discord.Member = None, *, reason = "none"):
-		title = "{} is Hitting Everyone with the Ban Hammer!".format(ctx.author.name) if target == None else "{} is Slamming {} into the Oblivion with the Ban Hammer!".format(ctx.author.name, target.name)
-		description = "The Hammer has been Polished! Time to Ban!" if reason == "none" else "The Hammer has been Polished! Time to Ban!\n**Reason:** {}".format(reason)
-		url = 'https://cdn.discordapp.com/attachments/771081877414346772/771990909138501642/tenor.gif'
+		title = "{} is Flaming everyone, Dodge !!".format(ctx.author.name) if target == None else "{} is Flaming {} into the Oblivion with the Ban Fire!".format(ctx.author.name, target.name)
+		description = "The Fuel Has been Injected ! Time to Ban !" if reason == "none" else "The Fuel Has been Injected ! Time to Ban !\n**Reason:** {}".format(reason)
+		url = 'https://cdn.discordapp.com/attachments/755740543358861383/772007520532168715/banpower.gif'
 		await self.fun_command_embed(ctx, title, description, url)
 	@ban.error
 	async def ban_error(self, ctx, error):
