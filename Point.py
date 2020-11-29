78b93752c90cdacd5b2c6fa3756d24d7fc5e8524
ZeusAbhijeet <62468954+ZeusAbhijeet@users.noreply.github.com>
2020-11-29 12:03:17 +0530

    Update Point.py

--- Point.py
+++ Point.py
@@ -9,9 +9,9 @@ class Point(commands.Cog):
 	def __init__(self, client):
 		self.client = client
 
-	@commands.command(name = 'give_points', aliases = ['give_coins'])
+	@commands.command(name = 'give_coins', aliases = ['give_points'])
 	@commands.has_role('SleepBot Admin')
-	async def give_points(self, ctx, target: discord.Member = None, amount = 0):
+	async def give_coins(self, ctx, target: discord.Member = None, amount = 0):
 		await Util.command_log(self.client, ctx, "give_points")
 		if target == None:
 			return
@@ -21,9 +21,11 @@ class Point(commands.Cog):
 			Util.POINT[target.id] = amount
 		await ctx.send(f'Gave {target} {amount} coins')
 	
-	@commands.command(name='points', aliases=['point', 'all_points', 'coins', 'all_coins'], help="Shows the Coins that the mentioned user has. If no one is mentioned then shows the Top 20 users.")
+	@commands.command(name='coins', 
+		aliases=['point', 'points', 'all_points', 'coin', 'all_coins'], 
+		help="Shows the Coins that the mentioned user has. If no one is mentioned then shows the Top 20 users.")
 	@Util.is_point_cmd_chnl()
-	async def points(self, ctx, target: discord.Member = None):
+	async def coins(self, ctx, target: discord.Member = None):
 		await Util.command_log(self.client, ctx, "points")
 		total_point = dict(Util.POINT)
 		for user in Util.DB_POINT:
@@ -54,8 +56,8 @@ class Point(commands.Cog):
 			if top_20 == 21:
 				break
 		await ctx.send(embed = embed)
-	@points.error
-	async def points_error(self, ctx, error):
+	@coins.error
+	async def coins_error(self, ctx, error):
 		if isinstance(error, commands.CheckFailure):
 			await ctx.send("Points command can only be used in <#{}> channel!".format(Util.POINTCMD))
 
