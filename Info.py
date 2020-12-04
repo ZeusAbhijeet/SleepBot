f6caf3e0aa8bd604c57834b22fff51977eef866d
ZeusAbhijeet <62468954+ZeusAbhijeet@users.noreply.github.com>
2020-12-04 23:22:42 +0530

    Add new command
    Added a How to Ask Command

--- Info.py
+++ Info.py
@@ -15,6 +15,58 @@ class Info(commands.Cog):
 		embed.set_footer(text=f'Requested by {ctx.author}')
 		await ctx.send(embed=embed)
 	
+	@commands.command(name='howtoask', help='Gives an explaination on how to ask a question')
+	async def howtoask(self, ctx):
+		embedcolour = random.randint(0, 0xffffff)
+		helpEmbed = discord.Embed(title = "How to ask",
+				description = "Here is a short explaination on how you should ask a question so that people are more likely to help you out:",
+				colour = embedcolour
+		)
+		helpEmbed.set_thumbnail(url="https://res.cloudinary.com/zeusabhijeet/image/upload/v1607099122/SleepBot/Info%20Commands/ask_question.png")
+		helpEmbed.set_author(name=ctx.message.author, icon_url=ctx.message.author.avatar_url)
+		helpEmbed.add_field(name="No Hello",
+			value="It's alright if you don't greet. You can directly ask your question right away. Saves your's and the other person's time. More on that [here](https://www.nohello.com/).",
+		)
+		helpEmbed.set_footer(text="Embed 1 of 2")
+		helpEmbed1 = discord.Embed(title = "Asking a Code Related Question",
+			description="""To ask a question on a code, refrain from sending screenshots or photos of the code as they are usually barely visible.
+			Here are two ways you can share your code:""",
+			colour = embedcolour)
+		helpEmbed1.set_thumbnail(url='https://res.cloudinary.com/zeusabhijeet/image/upload/v1607100500/SleepBot/Info%20Commands/howtoask_code.png')
+		helpEmbed1.add_field(name="1. Use an Online Code Sharing Service",
+			value="""
+				Below are some of the code sharing services that you can use: 
+				1. [GitHub Gist](https://gist.github.com/) 
+				2. [JSfiddle](https://jsfiddle.net/) 
+				3. [Codepen](https://codepen.io/) 
+				4. [Pastebin](https://pastebin.com/)
+				5. [OnlineGDB](https://www.onlinegdb.com/)
+				6. [repl.it](https://repl.it/)""",
+			inline = False)
+		helpEmbed1.add_field(name="2. Use a Code Snippet",
+			value="""To make a code snippet, encase your code between a pair of 3 backticks
+				
+				\```
+				Like This
+				\```
+				
+				So when a code is sent, it will look something like this:
+```cpp
+// C++ Hello World Program
+#include <iostream>
+using namespace std;
+int main() {
+	cout << 'Hello World!';
+	return 0;
+}
+```
+
+				You can read more about how Discord's Markdown works [here](https://gist.github.com/matthewzring/9f7bbfd102003963f9be7dbcf7d40e51).""",
+			inline = False)
+		helpEmbed1.set_footer(text="Embed 2 of 2")
+		await ctx.send(embed = helpEmbed)
+		await ctx.send(embed = helpEmbed1)
+
 	@commands.command(name='about', help='About the bot!')
 	async def about(self, ctx):
 		aboutEmbed = discord.Embed(title = "About SleepBot",
--- README.md
+++ README.md
@@ -1,44 +1,61 @@
 # Clinify Squad's SleepBot
+
 Official Bot for the Clinify.in Discord Server written in Python using Discord.py
 
 Though this bot is intended to be used only on the Clinify.in Discord Server, you can self host it in your server to test it out. Installation guide down below.
 
 ## Dependancies
+
 This bot requires the [Discord.py v1.5+ library](https://discordpy.readthedocs.io/) and [pretty-help 1.1.2](https://pypi.org/project/discord-pretty-help/) which can be installed using pip package manager.
 
 ## How to host the bot
+
 1. Edit the i-channel_table.txt file in SQL_CMDS folder as required.
 2. Run the SQLite3_Py.py script to make the database file.
 3. Create a .env file in the same directory as Main.py and enter your Discord Bot Token like this:
-```
+
+```py
 DISCORD_TOKEN=Your Token Here
 ```
+
 4. Run Main.py to start the bot!
 
 ## Note
+
 This bot requires Server Members Intent so make sure to enable that by going to your Discord Application -> Bot -> Privilaged Gateway Intents and enabling Server Members Intent. Without this, bot won't be able to access the cache to get user names.
 
 ## Changelogs
-08 Dec 2020:
+
+04 Dec 2020:
+
+1. Added "howtoask" command (Recommended by [Zircoz](https://github.com/Zircoz))
+
+03 Dec 2020:
+
 1. Added "Flip a Coin" command (Contributed by [parthivpatel1106](https://github.com/parthivpatel1106)).
 
 28 Nov 2020:
+
 1. Using PrettyHelp as default help command.
 2. Added Avatar command to show the avatar of mentioned user.
 3. Added an About command.
 4. Added a new prefix '?'.
 
 08 Nov 2020:
+
 1. Restricted points command to one channel. Running it in any other channel will show an error.
 2. Added new alises for points and give_points commands
 3. Added command logging. Bot will now send a message in the log channel with details about the command, the message content, message author, etc.
 
 ## Upcoming Changes
+
 1. Transition to MongoDB.
 2. Add command to share coins with other users
 
 ## Credits
+
 This bot contains code from the following Repos:
+
 1. [Robobert](https://github.com/JTexpo/Robobert): For the Point.py, Util.py and SQL_CMDS
 
 Special Thanks to [JTexpo](https://github.com/JTexpo) for helping me make this bot and letting me use parts of his code.
