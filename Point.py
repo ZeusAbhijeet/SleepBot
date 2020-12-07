d6e56e0c36ad9b4125bf0b04fca75292de66dff3
ZeusAbhijeet <62468954+ZeusAbhijeet@users.noreply.github.com>
2020-12-07 21:31:04 +0530

    Update Point.py

--- Point.py
+++ Point.py
@@ -22,8 +22,8 @@ class Point(commands.Cog):
 		await ctx.send(f'Gave {target} {amount} coins')
 	
 	@commands.command(name='coins', 
-		aliases=['point', 'points', 'all_points', 'coin', 'all_coins'], 
-		help="Shows the Coins that the mentioned user has. If no one is mentioned then shows the Top 20 users.")
+		aliases=['point', 'points', 'coin'], 
+		help="Shows the Coins that the mentioned user has.")
 	@Util.is_point_cmd_chnl()
 	async def coins(self, ctx, target: discord.Member = None):
 		await Util.command_log(self.client, ctx, "points")
@@ -33,7 +33,10 @@ class Point(commands.Cog):
 				total_point[user[0]] += user[1]
 			else:
 				total_point[user[0]] = user[1]
-		if target != None :
+		if target == None :
+			await ctx.send("Mention a user to check points.")
+			return
+		else:
 			if not (target.id in total_point):
 				total_point[target.id] = 0
 			embed = discord.Embed(title = "User {}'s Coins".format(target.name),
@@ -42,7 +45,23 @@ class Point(commands.Cog):
 				)
 			await ctx.send(embed = embed)
 			return
+	@coins.error
+	async def coins_error(self, ctx, error):
+		if isinstance(error, commands.CheckFailure):
+			await ctx.send("Points command can only be used in <#{}> channel!".format(Util.POINTCMD))
 	
+	@commands.command(name='top', 
+		aliases=['leaderboard', 'all_coins', 'all_points'], 
+		help="Shows the top 20 users in leaderboard.")
+	@Util.is_point_cmd_chnl()
+	async def top(self, ctx):
+		await Util.command_log(self.client, ctx, "top")
+		total_point = dict(Util.POINT)
+		for user in Util.DB_POINT:
+			if user[0] in total_point:
+				total_point[user[0]] += user[1]
+			else:
+				total_point[user[0]] = user[1]
 		top_20 = 1
 		total_point = sorted(total_point.items(), key = lambda kv: kv[1])
 		total_point.reverse()
@@ -56,12 +75,10 @@ class Point(commands.Cog):
 			if top_20 == 21:
 				break
 		await ctx.send(embed = embed)
-	@coins.error
-	async def coins_error(self, ctx, error):
+	@top.error
+	async def top_error(self, ctx, error):
 		if isinstance(error, commands.CheckFailure):
 			await ctx.send("Points command can only be used in <#{}> channel!".format(Util.POINTCMD))
-
-
 
 def setup(client):
 	client.add_cog(Point(client))
