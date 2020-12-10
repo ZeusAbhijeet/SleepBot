38eaab6645e7c98afb738e644f4396c69b388bbe
YogPanjarale <suryakant.panjarale@gmail.com>
2020-12-10 20:33:24 +0530

    CodeHelp ,"ask" added with empty command error

---
+++ CodeHelp.py
@@ -0,0 +1,71 @@
+import discord
+import random 
+from discord.ext import commands
+import aiohttp
+import typing 
+import json
+
+class CodeHelp(commands.Cog):
+    def __init__(self,client):
+        self.client = client;
+    
+    @commands.command('ask',help='ask a questions ')
+    async def ask(self,ctx, result_limit: typing.Optional[int] = 1, *, term: str):
+        try:
+            answer=''
+            googlequery=term
+            q=googlequery.replace(" ","+")
+            searchurl='https://www.google.com/search?q='+q
+            print(searchurl,q)
+            # print(term)
+            async with aiohttp.ClientSession() as session:
+                async with session.get('https://www.codegrepper.com/api/search.php', params ={"q":term}) as r :
+                    result = await r.json()
+                
+                data=result['answers']
+                for i in data:
+                    # print(i)
+                    # print(i['answer'])
+                    ans = i['answer']
+                    lang =i['language']
+                    answer+=f'```{lang}\n{ans}```'
+            embed = discord.Embed(
+                title ="You asked",
+                description =f'{term} \n ',
+                colour=random.randint(0, 0xffffff)
+            )
+            
+            # embed.set_footer(text=f'{ctx.message}')
+            
+            if len(answer)==0:
+                notFoundEmbed=discord.Embed(
+                    title="Answer Not Found",
+                    description=f'[Search yourself]({searchurl})'
+                )
+                await ctx.send(embed=embed)
+                await ctx.send(embed=notFoundEmbed)
+                pass
+            else:
+                await ctx.send(answer)
+                pass
+            notGotEmbed=discord.Embed(
+                title=":frowning2: Not Got Your Answer?",
+                description=f'[Search yourself]({searchurl}) [OnlineGDB](https://www.onlinegdb.com/)'
+            )
+        except TypeError :
+            print('Error')
+            pass
+        finally:  
+            noargEmbed=discord.Embed(
+                    title="Ask Something, it can't be blank",
+                    description='''
+                    something expected 
+                    `?ask what you want to ask`
+                    '''
+                )
+            await ctx.send(embed=noargEmbed)
+        # await ctx.send(answer)
+
+
+def setup(client):
+    client.add_cog(CodeHelp(client))
