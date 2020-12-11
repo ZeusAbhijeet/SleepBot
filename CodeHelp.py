5d97925db3e4b6118de99d2bf24df4602fab0a21
YogPanjarale <suryakant.panjarale@gmail.com>
2020-12-11 13:01:29 +0530

    added embeds for answer

--- CodeHelp.py
+++ CodeHelp.py
@@ -17,40 +17,52 @@ class CodeHelp(commands.Cog):
             q=googlequery.replace(" ","+")
             searchurl='https://www.google.com/search?q='+q
             print(searchurl,q)
+
+            embed = discord.Embed(
+                title ="You asked",
+                description =f'{term} \n ',
+                colour=random.randint(0, 0xffffff)
+            )
             # print(term)
             async with aiohttp.ClientSession() as session:
                 async with session.get('https://www.codegrepper.com/api/search.php', params ={"q":term}) as r :
                     result = await r.json()
                 
                 data=result['answers']
+                answerEmbed=discord.Embed(
+                    title='Answers',
+                )
                 for i in data:
                     # print(i)
                     # print(i['answer'])
                     ans = i['answer']
                     lang =i['language']
+                    source=i['source_id']
                     answer+=f'```{lang}\n{ans}```'
-            embed = discord.Embed(
-                title ="You asked",
-                description =f'{term} \n ',
-                colour=random.randint(0, 0xffffff)
-            )
+
+                    answerEmbed.add_field(
+                        name="name",
+                        value=f'```{lang}\n {ans}```'+f'\n [source]({source})'
+                    )
             
             # embed.set_footer(text=f'{ctx.message}')
             
             if len(answer)==0:
                 notFoundEmbed=discord.Embed(
                     title="Answer Not Found",
-                    description=f'[Search yourself]({searchurl})'
+                    description=f''' You can also contribute to this answers by intalling [codegrepper](https://www.codegrepper.com/) Extensions and marking answer when you find it
+                    \n[Search yourself]({searchurl})'''
                 )
                 await ctx.send(embed=embed)
                 await ctx.send(embed=notFoundEmbed)
                 pass
             elif len(answer)>1:
                 await ctx.send(embed=embed)
-                await ctx.send(answer)
+                await ctx.send(embed=answerEmbed)
                 notGotEmbed=discord.Embed(
                 title=":frowning2: Not Got Your Answer?",
-                description=f'[Search yourself]({searchurl})'
+                description=f''' You can also contribute to this answers by intalling [codegrepper](https://www.codegrepper.com/) Extensions and marking answer when you find it
+                \n[Search yourself]({searchurl})'''
                 )
                 await ctx.send(embed=notGotEmbed)
                 pass
