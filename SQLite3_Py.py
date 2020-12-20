1c03a3dc74800593c07c35988a228ea8e2c9ffef
ZeusAbhijeet <62468954+ZeusAbhijeet@users.noreply.github.com>
2020-12-21 00:42:17 +0530

    Add new stuff

--- SQLite3_Py.py
+++ SQLite3_Py.py
@@ -9,10 +9,16 @@ if __name__ == '__main__':
 	sql_cmd = open("./SQL_CMDS/c-channel_table.txt", 'r').read()
 	c.execute(sql_cmd)
 	print("Created Channel Table")
+	sql_cmd = open("./SQL_CMDS/c-rule_table.txt", 'r').read()
+	c.execute(sql_cmd)
+	print("Created Rule Table")
 
 	sql_cmd = open("./SQL_CMDS/i-channel_table.txt", 'r').read()
 	c.execute(sql_cmd)
 	print("Init into channel_table")
+	sql_cmd = open("./SQL_CMDS/i-rule_table.txt", 'r').read()
+	c.execute(sql_cmd)
+	print("Init into Rule Table")
 
 	conn.commit()
 	conn.close()
