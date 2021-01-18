ac7f172eceffa49fd653353e9debaf8b9d24d038
ZeusAbhijeet <62468954+ZeusAbhijeet@users.noreply.github.com>
2021-01-18 10:49:44 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -31,7 +31,7 @@ async def on_ready():
 	for server in client.guilds:
 		print(f'Guild name: {server.name}')
 		print(f'Guild ID: {server.id}')
-	await client.change_presence(activity=discord.Game(name="with gifts | ?help"))
+	await client.change_presence(activity=discord.Game(name="with coins | ?help"))
 
 if __name__ == '__main__':
 	extensions = {'Info', 'Point', 'Fun', 'Mod', 'CodeHelp', 'Rule'} 
